0447f3602c5de1dc16ae76328638dcb27f0469ab
Bruno Russo <bruno@jumpstartmd.com>
2023-10-25 23:23:20 -0300

    fix: change some logics and round the change

--- main.py
+++ main.py
@@ -23,10 +23,10 @@ class coffee_machine:
 
     def process_payment(self, beverage_cost) -> bool:
         print("Please insert coins.")
-        quarters = int(input("How many quarters?: "))
-        dimes = int(input("How many dimes?: "))
-        nickles = int(input("How many nickles?: "))
-        pennies = int(input("How many pennies?: "))
+        quarters = int(input("How many quarters?: ")) * 0.25
+        dimes = int(input("How many dimes?: ")) * 0.10
+        nickles = int(input("How many nickles?: ")) * 0.05
+        pennies = int(input("How many pennies?: ")) * 0.01
 
         total_money = quarters + dimes + nickles + pennies
 
@@ -34,7 +34,7 @@ class coffee_machine:
             self.profit += beverage_cost
             change = total_money - beverage_cost
             if change > 0:
-                print(f"Here is ${change} in change")
+                print(f"Here is ${round(change, 2)} in change")
             return True
         else:
             print("Sorry that's not enough money. Money refunded")
@@ -44,31 +44,20 @@ class coffee_machine:
         try:
             beverage = MENU[beverage_name]
             beverage_ingredients = beverage['ingredients']
-            if self.resources_are_available(beverage_ingredients, hasattr(beverage_ingredients, 'milk')):
+            if self.resources_are_available(beverage_ingredients):
                 if self.process_payment(beverage['cost']):
                     self.prepare_beverage(beverage_ingredients)
                     print(f"Here is your {beverage_name}, Enjoy it!")
         except Exception as error:
             print(f"Beverage not found {error}")
 
-    def resources_are_available(self, beverage_ingredients, has_milk_in_recipe) -> bool:
+    def resources_are_available(self, beverage_ingredients) -> bool:
         can_make_beverage = True
-        has_water = beverage_ingredients['water'] <= resources['water']
-        has_coffee = beverage_ingredients['coffee'] <= resources['coffee']
-        
-        if not has_water:
-            can_make_beverage = False
-            print("Sorry there is not enough water")
-                
-        if not has_coffee:
-            can_make_beverage = False
-            print("Sorry there is not enough coffee")
-
-        if has_milk_in_recipe:
-            has_milk = beverage_ingredients['milk'] <= resources['milk']
-            if not has_milk:
+       
+        for ingredient in beverage_ingredients:
+            if resources[ingredient] < beverage_ingredients[ingredient]:
+                print(f"Sorry there is not enough {ingredient}")
                 can_make_beverage = False
-                print("Sorry there is not enough milk")
         
         return can_make_beverage
 
